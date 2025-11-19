4c8428d4c9ae3677994812c34a6c6203d9d47e57
Qinyue Tong <152782886+Edisonhimself@users.noreply.github.com>
2025-11-19 14:18:19 +0800

    Update inference_one_image.sh

--- inference_one_image.sh
+++ inference_one_image.sh
@@ -12,4 +12,5 @@ CUDA_VISIBLE_DEVICES=0 deepspeed --master_port=24998 ./inference_one_image.py \
 --sample_rates="7,2" \
 --exp_name="xxxxxx" \
 --conv_type="mistral_instruct" \
+--weight="" \
 --eval_only 
